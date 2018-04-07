a550c9ac59c2ce5fd12c2bd6cbaa9f982d6a8fb2
neetshah <shahn17@mcmaster.ca>
2018-04-07 01:47:19 -0400

    can grab blogs from the DB using id

--- terminal_blog/models/post.py
+++ terminal_blog/models/post.py
@@ -1,16 +1,18 @@
 __author__ = 'shahn17'
 
+import uuid
+import datetime
 from ..database import Database
 
 class Post(object):
 
-    def __init__(self, blog_id, title, content, date, author, id):
+    def __init__(self, blog_id, title, content, author, date=datetime.datetime.utcnow(), id=None):
         self.blog_id = blog_id
-        self.title = "Title"
-        self.content = "Content"
-        self.author = "Neet"
+        self.title = title
+        self.content = content
+        self.author = author
         self.created_date = date
-        self.id = id
+        self.id = uuid.uuid4().hex if id is None else id
 
     def save_to_mongo(self):
         Database.insert(collection='posts', data=self.json())
